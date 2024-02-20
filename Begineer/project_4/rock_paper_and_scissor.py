4cce3d2f6f49e1315cfc6cf8249ea37dbed690e8
Raman-Bansal-9034 <abhay58920@gmail.com>
2024-02-20 19:37:45 +0530

    Refactor the code a little bit.

--- Begineer/project_4/rock_paper_and_scissor.py
+++ Begineer/project_4/rock_paper_and_scissor.py
@@ -7,29 +7,24 @@ game_keyword = ("rock", "paper", "scissor")
 print(hello)
 
 user_choice = int(input("What do you choose ? Type 0 for Rock, 1 For paper, and 2 for Scissor "))
-choice = ""
 
 if user_choice > 2 or user_choice < 0:
-    print("You typed a Wrong number. You loose.")
+    print("You chose the wrong option.")
 else:
-    print(f"\nYou chooses: {game_keyword[user_choice]}")
-    print(game_images[user_choice])
+    print(f"\nYou choose: {game_keyword[user_choice]} \n {game_images[user_choice]}")
 
     computer_choice = random.randint(0, 2)
 
-    print(f"Computer chooses: {game_keyword[computer_choice]} \n {game_images[computer_choice]}")
+    print(f"Computer choose: {game_keyword[computer_choice]} \n {game_images[computer_choice]}")
 
     if user_choice == computer_choice:
-        print("Match Draw.")
-    elif user_choice == 0 and computer_choice == 1:
-        print("You Lose! Computer Wins.")
+        print("It's a Draw.")
     elif user_choice == 0 and computer_choice == 2:
         print("You Win! Computer Loose.")
-    elif user_choice == 1 and computer_choice == 0:
-        print("You Win! Computer Loose.")
-    elif user_choice == 1 and computer_choice == 2:
-        print("You Loose! Computer Wins.")
     elif user_choice == 2 and computer_choice == 0:
         print("You Loose! Computer Wins.")
-    elif user_choice == 2 and computer_choice == 1:
+    elif user_choice > computer_choice:
         print("You Win! Computer Loose.")
+    elif user_choice < computer_choice:
+        print("You Loose! Computer Wins.")
+
