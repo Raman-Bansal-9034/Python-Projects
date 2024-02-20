ef356d583009728d1f614f180ca7e2feb9571d67
Raman-Bansal-9034 <abhay58920@gmail.com>
2024-02-20 19:24:02 +0530

    Updated project_4 with a new condition.

--- Begineer/project_4/rock_paper_and_scissor.py
+++ Begineer/project_4/rock_paper_and_scissor.py
@@ -9,29 +9,27 @@ print(hello)
 user_choice = int(input("What do you choose ? Type 0 for Rock, 1 For paper, and 2 for Scissor "))
 choice = ""
 
-if user_choice > 2:
+if user_choice > 2 or user_choice < 0:
     print("You typed a Wrong number. You loose.")
 else:
     print(f"\nYou chooses: {game_keyword[user_choice]}")
     print(game_images[user_choice])
 
-computer_choice = random.randint(0, 2)
+    computer_choice = random.randint(0, 2)
 
-print(f"Computer chooses: {game_keyword[computer_choice]} \n {game_images[computer_choice]}")
+    print(f"Computer chooses: {game_keyword[computer_choice]} \n {game_images[computer_choice]}")
 
-if user_choice >= 3 or user_choice < 0:
-    print("You didn't choice the right option. Try again.")
-elif user_choice == computer_choice:
-    print("Match Draw.")
-elif user_choice == 0 and computer_choice == 1:
-    print("You Lose! Computer Wins.")
-elif user_choice == 0 and computer_choice == 2:
-    print("You Win! Computer Loose.")
-elif user_choice == 1 and computer_choice == 0:
-    print("You Win! Computer Loose.")
-elif user_choice == 1 and computer_choice == 2:
-    print("You Loose! Computer Wins.")
-elif user_choice == 2 and computer_choice == 0:
-    print("You Loose! Computer Wins.")
-elif user_choice == 2 and computer_choice == 1:
-    print("You Win! Computer Loose.")
+    if user_choice == computer_choice:
+        print("Match Draw.")
+    elif user_choice == 0 and computer_choice == 1:
+        print("You Lose! Computer Wins.")
+    elif user_choice == 0 and computer_choice == 2:
+        print("You Win! Computer Loose.")
+    elif user_choice == 1 and computer_choice == 0:
+        print("You Win! Computer Loose.")
+    elif user_choice == 1 and computer_choice == 2:
+        print("You Loose! Computer Wins.")
+    elif user_choice == 2 and computer_choice == 0:
+        print("You Loose! Computer Wins.")
+    elif user_choice == 2 and computer_choice == 1:
+        print("You Win! Computer Loose.")
