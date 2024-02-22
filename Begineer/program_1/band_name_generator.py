888c16834bb1e28467ccfba9691f8c00ee303b74
Raman-Bansal-9034 <abhay58920@gmail.com>
2024-02-22 19:52:27 +0530

    updated program hierarchy.

---
+++ Begineer/program_1/band_name_generator.py
@@ -0,0 +1,9 @@
+print("\nWelcome to the Band Name Generator Program.\n")
+
+city = input("What's name of the city you grew up in ? ")
+
+name_of_pet = input("Enter name of your pet. ")
+
+print("\nName of your band could be : " + city + " " + name_of_pet)
+
+
