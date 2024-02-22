a557735490406227d139fe82a1d9001913aaf88a
Raman-Bansal-9034 <abhay58920@gmail.com>
2024-02-22 20:10:05 +0530

    Created 6th program: Escaping The Maze

--- Begineer/program_6/escaping_the_maze.py
+++ Begineer/program_6/escaping_the_maze.py
@@ -1,5 +1,6 @@
 # https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze
 # &url=worlds%2Ftutorial_en%2Fmaze1.json
+
 # go to this url remove all the methods that contain pass keyword and run the below code. It will work fine.
 
 def move():
