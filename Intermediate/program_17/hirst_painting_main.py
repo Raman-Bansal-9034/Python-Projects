9628cdc214e0b7185af67f55a67049d08aec7d21
Raman-Bansal-9034 <abhay58920@gmail.com>
2024-03-11 14:34:00 +0530

    Created 17th program: Hirst Painting

---
+++ Intermediate/program_17/hirst_painting_main.py
@@ -0,0 +1,43 @@
+import colorgram
+import turtle
+import random
+
+turtle.colormode(255)
+screen = turtle.Screen()
+tim = turtle.Turtle()
+
+# color_list = [(202, 164, 110), (149, 75, 50), (222, 201, 136), (53, 93, 123), (170, 154, 41), (138, 31, 20),
+#               (134, 163, 184), (197, 92, 73), (47, 121, 86), (73, 43, 35), (145, 178, 149), (14, 98, 70),
+#               (232, 176, 165), (160, 142, 158), (54, 45, 50), (101, 75, 77), (183, 205, 171), (36, 60, 74),
+#               (19, 86, 89), (82, 148, 129), (147, 17, 19), (27, 68, 102), (12, 70, 64), (107, 127, 153),
+#               (176, 192, 208), (168, 99, 102)]
+
+rgb_colors = []
+colors = colorgram.extract('image.jpg', 30)
+for color in colors:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    new_color = (r, g, b)
+    rgb_colors.append(new_color)
+print(rgb_colors)
+
+tim.speed("fastest")
+tim.penup()  # To hide the line that are drawn when the turtle create the dots or move from one position to another.
+tim.hideturtle()  # To hide our turtle so that it doesn't appear when we are drawing dots.
+tim.setheading(220)
+tim.forward(300)
+tim.setheading(0)
+number_of_dots = 100
+
+for dot_count in range(1, number_of_dots + 1):
+    tim.dot(20, random.choice(rgb_colors))
+    tim.forward(50)
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
+screen.exitonclick()
